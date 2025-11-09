bdda322d35b16ff65f2c91e1e4f8b8ef6a88339a
diego <diego056790@gmail.com>
2025-11-09 14:58:49 +0100

    adedd a footer

--- Page/src/App.jsx
+++ Page/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import wildleaf_logo from "./assets/wildleaf_logo.png"
 import './App.css'
+import Footer from "./assets/Footer.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <img src={wildleaf_logo} alt="wildleaf_logo" />
+        <Footer></Footer>
     </>
   )
 }
---
+++ Page/src/assets/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return (
+        <div className="Footer">
+            <hr></hr>
+            <h2>WildLeaf</h2>
+            <p>Project made in U-tad</p>
+
+        </div>
+    );
+}
+export default Footer
