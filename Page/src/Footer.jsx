948372b80e11db596b6c2d07062b07398d71ead2
diego <diego056790@gmail.com>
2025-11-09 16:29:06 +0100

    made the footer look cool

--- Page/src/Footer.jsx
+++ Page/src/Footer.jsx
@@ -1,14 +1,16 @@
+import utadlogo from './assets/utadLogo.png'
 function Footer(){
     return (
         <div className="Footer">
             <hr></hr>
-            <h2>WildLeaf</h2>
+            <h2 className="footerheader">WildLeaf</h2>
             <p>Project made in U-tad</p>
             <ul className="footerList">
                 <li className="footerItem">Contact</li>
                 <li className="footerItem">About us</li>
                 <li className="footerItem">Collaborators</li>
             </ul>
+            <img src={utadlogo} className="footerlogo"></img>
         </div>
     );
 }
