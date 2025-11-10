dc5e32590d59a225fda6444aaca38d8086a1510d
dubiefe <emiliedubief@free.fr>
2025-11-10 16:20:56 +0100

    Styling the details

--- Page/src/details_component/details.jsx
+++ Page/src/details_component/details.jsx
@@ -7,15 +7,15 @@ function Details() {
       <div id="details_container">
         <div id='problem_container'>
             <h2>What is the problem faced?</h2>
-            <div>
-                <div>
+            <div class="first_container">
+                <div class="content_container">
                     <p>Most people only know</p>
                     <h3>300</h3>
                     <p>of all plants that exists</p>
                     <p>over</p>
                     <h3>350.000</h3>
                 </div>
-                <div>
+                <div class="content_container">
                     <p>Today in Spain we count</p>
                     <h3>132</h3>
                     <p>endangered plants</p>
@@ -26,25 +26,36 @@ function Details() {
         </div>
         <div id='what_container'>
             <h2>What is WildLeaf?</h2>
-            <p>WildLeaf is an interractive map of all endangered or vulnerable plants of Spain.</p>
-            <p>With WildLeaf you can:</p>
-            <ul>
-                <li>Search plants according to the region</li>
-                <li>Learn what are their roles in the environment</li>
-                <li>Learn how to protect them</li>
-                <li>Display the cleaning you made to save the envirnment</li>
-            </ul>
+            <p>WildLeaf is an interractive map of all endangered or vulnerable plants of Spain, it offers the possibility to do:</p>
+            <div class="first_container">
+                <div class="second_container">
+                    <div class="content_container">
+                        <p>Search plants according to the region</p>
+                    </div>
+                    <div class="content_container">
+                        <p>Learn what are their roles in the environment</p>
+                    </div>
+                </div>
+                <div class="second_container">
+                    <div class="content_container">
+                        <p>Learn how to protect them</p>
+                    </div>
+                    <div class="content_container">
+                        <p>Display the cleaning you made to save the envirnment</p>
+                    </div>
+                </div>
+            </div>
         </div>
         <div id='why_container'>
             <h2>Why are we making the difference?</h2>
-            <div>
-                <div>
+            <div class="first_container">
+                <div class="content_container">
                     <p>Reliable data</p>
                 </div>
-                <div>
+                <div class="content_container">
                     <p>Simplified Information</p>
                 </div>
-                <div>
+                <div class="content_container">
                     <p>Accessible to everyone</p>
                 </div>
             </div>
