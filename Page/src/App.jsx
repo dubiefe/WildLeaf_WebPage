807f9b37b40b43b743e4598ee2b58d118f3ac65b
dubiefe <emiliedubief@free.fr>
2025-11-10 09:13:46 +0100

    References

--- Page/src/App.jsx
+++ Page/src/App.jsx
@@ -4,6 +4,7 @@ import Nav from "./nav_component/nav.jsx";
 import Presentation from './presentation_component/presentation.jsx';
 import Details from './details_component/details.jsx';
 import Video from './video_component/video.jsx';
+import References from './references_component/references.jsx';
 import Footer from "./footer_component/footer.jsx";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Presentation/>
       <Details/>
       <Video/>
+      <References/>
       <Footer/>
     </>
   )
