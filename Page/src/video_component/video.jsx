c632ba64661bc9a6d9056d2f8cb95a28b84c2bb0
dubiefe <emiliedubief@free.fr>
2025-11-11 09:50:04 +0100

    Update video

--- Page/src/video_component/video.jsx
+++ Page/src/video_component/video.jsx
@@ -1,18 +1,17 @@
 import './video.css'
-import wildleaf_demo from "../assets/wildleaf_demo.mp4"
 
 function Video() {
 
   return (
     <>
       <div id="video_container">
-        <iframe width="560" height="315"
-          src="https://www.youtube.com/embed/aas3PvUhaa8"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen>
-      </iframe>
+          <iframe width="560" height="315"
+            src="https://www.youtube.com/embed/aas3PvUhaa8"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen>
+        </iframe>
       </div>
     </>
   )
