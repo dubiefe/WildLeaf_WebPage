56f936fb4a196218037bbb6f44387b10f3f27c47
dubiefe <emiliedubief@free.fr>
2025-11-11 09:49:30 +0100

    Update Video

--- Page/src/video_component/video.jsx
+++ Page/src/video_component/video.jsx
@@ -6,9 +6,13 @@ function Video() {
   return (
     <>
       <div id="video_container">
-        <video width="320" height="240" controls>
-            <source src="https://youtu.be/aas3PvUhaa8" type="video/mp4" />
-        </video>
+        <iframe width="560" height="315"
+          src="https://www.youtube.com/embed/aas3PvUhaa8"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen>
+      </iframe>
       </div>
     </>
   )
